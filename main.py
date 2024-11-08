528eea902475672e6871f3161f8f2d31d9c7c1fd
Sandeep Pradhan <sandeeppradhan@Sandeeps-MacBook-Air.local>
2024-11-08 10:58:00 +0530

    Add all the forms

--- main.py
+++ main.py
@@ -3,11 +3,28 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String
+import os
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from forms import (
+    VisualInspection, 
+    KaptonGluing, 
+    HvIvForm, 
+    SensorGluing, 
+    NeedleMetrologyForm, 
+    SkeletonTestForm, 
+    HybridGluingForm, 
+    WireBondingForm, 
+    NoiseTestForm, 
+    BufNimForm
+)
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-
+import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
-
+app.config["UPLOAD_FOLDER"]= "static/uploads/station_image"
+ckeditor = CKEditor(app)
+Bootstrap5(app)
 # CREATE DATABASE
 
 
@@ -15,7 +32,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nisers.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -23,10 +40,24 @@ db.init_app(app)
 
 
 class User(UserMixin ,db.Model):
+    __tablename__ = "Users"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
+
+class Station(db.Model):
+    __tablename__ = "Stations"
+    id: Mapped[int] = mapped_column(Integer ,primary_key=True)
+    station_name: Mapped[str] = mapped_column(String(250),unique=True,nullable =False)
+    description: Mapped[str]=mapped_column(String(250) ,nullable =False)
+    year:Mapped[int] =mapped_column(nullable =False)
+    day:Mapped[int] =mapped_column(nullable =False)
+    month:Mapped[int] =mapped_column(nullable =False)
+    img_url:Mapped[str]=mapped_column(String(250) ,nullable =False)
+
+
+
 # Flask log-in
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -72,9 +103,36 @@ def login():
 @app.route('/secrets')
 @login_required
 def secrets():
-    return render_template("secrets.html")
+    return render_template("workflow.html")
 
 
+@app.route('/stations')
+@login_required
+def stations():
+    with app.app_context():
+        result = db.session.execute(db.select(Station).order_by(Station.id))
+        all_stations = result.scalars().all()
+        return render_template("all_stations.html",all_stations = all_stations)
+    #return render_template("stations1.html")
+@app.route("/station_form")
+@login_required
+def show_form():
+    return render_template("station_form.html")
+@app.route("/add_station",methods=["POST"])
+@login_required
+def add_station():
+    print("we have entered the upload_image")
+    image = request.files["image"]
+    station_name = request.form["name"]
+    description = request.form["comment"]
+    date = datetime.datetime.now()
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+    image.save(file_path)
+    print(file_path)
+    new_station = Station(station_name = station_name ,year = date.year , day = date.day , month = date.month ,img_url = file_path,description = description)
+    db.session.add(new_station)
+    db.session.commit()
+    return redirect(url_for('stations'))
 @app.route('/logout')
 def logout():
     logout_user()
@@ -84,6 +142,36 @@ def logout():
 @app.route('/download')
 def download():
     return send_from_directory("static", path="files/cheat_sheet.pdf")
+
+@app.route('/add_data', methods=["GET", "POST"])
+def add_data():
+    num = request.args.get('num')    
+    step_no = int(num)
+    if step_no == 1:
+        form = VisualInspection()
+    elif step_no == 2:
+        form = KaptonGluing()
+    elif step_no == 3:
+        form = HvIvForm()
+    elif step_no == 4:
+        form = SensorGluing()
+    elif step_no == 5:
+        form = NeedleMetrologyForm()
+    elif step_no == 6:
+        form = SkeletonTestForm()
+    elif step_no == 7:
+        form = HybridGluingForm()
+    elif step_no == 8:
+        form = WireBondingForm()
+    elif step_no == 9:
+        form = NoiseTestForm()
+    elif step_no == 10:
+        form = BufNimForm()
+    if form.validate_on_submit():
+        # Handle the form submission logic here
+        pass
+    return render_template("visual_inspection.html", form=form)
+
 
 
 if __name__ == "__main__":
